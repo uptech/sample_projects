be0aacfd3af3cbe6d1d9ec5840dedd802bdfae3b
Ryan Hedges <ryanhedges@gmail.com>
2018-11-09 15:10:56 -0800

    Initial start of uinavigationbar-under-statusbar project
    This includes the start of the project for the uinavigationbar that will
    be used in our blog post.
    
    This adds a `.gitignore` that should cover the iOS projects so we don't
    commit anything we shouldn't.
    
    This is an app without a storyboard so I have removed as much cruft as I
    could to give you a good clean place to start. When you run the app you
    should see a view that reminds you of UpTech.

---
+++ ios/uinavigationbar-under-statusbar/uinavigationbar-under-statusbar/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.init(red: 187/255, green: 42/255, blue: 97/255, alpha: 100)
+    }
+
+
+}
+
