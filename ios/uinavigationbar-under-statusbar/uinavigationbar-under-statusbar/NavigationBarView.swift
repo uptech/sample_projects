49262a2e172f8f8ff7fdd0700f3699a00c6ba6f9
Ryan Hedges <ryanhedges@gmail.com>
2018-11-09 15:56:38 -0800

    This is where the magic happens for making it extend
    Extend the `UIBarPositioningDelegate` to override the barPosition to
    `.toAttached`. That's where the magic comes from.

--- ios/uinavigationbar-under-statusbar/uinavigationbar-under-statusbar/NavigationBarView.swift
+++ ios/uinavigationbar-under-statusbar/uinavigationbar-under-statusbar/NavigationBarView.swift
@@ -3,3 +3,10 @@ import UIKit
 class NavigationBarView: UINavigationBar {
     // Do any other custom things you might need to do for this UINavigationBar
 }
+
+extension NavigationBarView: UIBarPositioningDelegate {
+    // Make the navigation bar extend ender status bar
+    override var barPosition: UIBarPosition {
+        return .topAttached
+    }
+}
