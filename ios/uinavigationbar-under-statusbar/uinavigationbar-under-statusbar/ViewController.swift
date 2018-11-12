f7d7c4dede1e6455003516d6e8a40bd43cc602b9
Andrew De Ponte <cyphactor@gmail.com>
2018-11-12 13:16:56 -0800

    Switch to do the navbar the way apple intended
    I did this so that the example would be done the way apple intended that
    property to be set when it is not managed by a controller.

--- ios/uinavigationbar-under-statusbar/uinavigationbar-under-statusbar/ViewController.swift
+++ ios/uinavigationbar-under-statusbar/uinavigationbar-under-statusbar/ViewController.swift
@@ -2,9 +2,8 @@ import UIKit
 import Constraid
 
 class ViewController: UIViewController {
-
-    private let navigationBarView: NavigationBarView = {
-        let nav = NavigationBarView(frame: .zero)
+    private let navigationBarView: UINavigationBar = {
+        let nav = UINavigationBar(frame: .zero)
 
         // Add items to the left and right of the navigation bar.
         // This isn't necessary to do but we wanted you to see something expected in the navigation bar
@@ -27,6 +26,16 @@ class ViewController: UIViewController {
         print("Save Clicked")
     }
 
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        navigationBarView.delegate = self
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.init(red: 187/255, green: 42/255, blue: 97/255, alpha: 100)
@@ -38,7 +47,10 @@ class ViewController: UIViewController {
         // safeAreaLayoutGuide will be under the status bar and the notch in the iPhone X
         Constraid.cup(navBar, byTopEdgeOf: view.safeAreaLayoutGuide).activate()
     }
-
-
 }
 
+extension ViewController: UINavigationBarDelegate {
+    public func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
